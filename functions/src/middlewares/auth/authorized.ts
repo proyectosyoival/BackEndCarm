370d376f831761581410a20d146ed3e8324c45dd
Alejandro <jaime.alejandro.ruvalcaba@gmail.com>
2020-06-23 00:26:02 -0500

    Espacio Ejemplo

--- functions/src/middlewares/auth/authorized.ts
+++ functions/src/middlewares/auth/authorized.ts
@@ -6,7 +6,7 @@ Creador: Ruben Gonzalez
 Fecha: 19/06/2020
 Desc: Validamos que la ID en los parámetros de solicitud sea la misma 
 como el que está en el token de autenticación. 
-Si el usuario no tiene el rol requerido, le devolveremos un 403.*/
+Si el usuario no tiene el rol requerido, le devolveremos un 403. */
 /***********************************************************************/
 
 export function isAuthorized(opts: { hasRole: Array<'admin' | 'user'>, allowSameUser?: boolean }) {
